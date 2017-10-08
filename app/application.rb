d95219eec50999c1441b03c39079235ed51a9b0d
buckethead1986 <rspeckpfs@gmail.com>
2017-10-08 10:44:08 -0400

    Done.

---
+++ app/application.rb
@@ -0,0 +1,20 @@
+class Application
+  def call(env)
+    resp = Rack::Response.new
+
+    afternoon = Time.parse "12:00 pm"
+
+    current_time = Time.now
+    resp.write "The current time is #{current_time}\n"
+    resp.write "The cutoff to 'afternoon' is #{afternoon}\n"
+    if current_time < afternoon
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
+    end
+
+    resp.finish
+
+  end
+
+end
